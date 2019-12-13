c5a00820da641618d8670492100832dda654c4fa
Samuel Alves <samuel241296@hotmail.com>
2019-12-13 13:38:50 -0300

    preparando pra deploy

--- src/main/java/br/com/samuel/cartorio/config/JPAConfiguration.java
+++ src/main/java/br/com/samuel/cartorio/config/JPAConfiguration.java
@@ -3,8 +3,10 @@ package br.com.samuel.cartorio.config;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -15,36 +17,48 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class JPAConfiguration {
 	
-	 @Bean
-	    public JpaTransactionManager transactionManager(EntityManagerFactory emf){
-	        return new JpaTransactionManager(emf);
-	    }
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, 
+				Properties additionalProperties) {
+		
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		factoryBean.setJpaVendorAdapter(vendorAdapter);
+
+		factoryBean.setPackagesToScan("br.com.samuel.cartorio.models");
+		
+		factoryBean.setDataSource(dataSource);
+		factoryBean.setJpaProperties(additionalProperties);
+		
+		return factoryBean;
+	}
+
+	@Bean
+	@Profile("dev")
+	public Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		return properties;
+	}
+
+	@Bean
+	@Profile("dev")
+	private DriverManagerDataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername("root");
+		dataSource.setPassword("121314");
+		dataSource.setUrl("jdbc:mysql://localhost/cartorio");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		return dataSource;
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		return new JpaTransactionManager(emf);
+	}
 	
-	 @Bean
-	    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-	        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-	        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-
-	        factoryBean.setJpaVendorAdapter(vendorAdapter);
-
-	        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	        dataSource.setUsername("root");
-	        dataSource.setPassword("121314");
-	        dataSource.setUrl("jdbc:mysql://localhost:3306/cartorio");
-	        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-
-	        factoryBean.setDataSource(dataSource);
-
-	        Properties props = new Properties();
-	        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-	        props.setProperty("hibernate.show_sql", "true");
-	        props.setProperty("hibernate.hbm2ddl.auto", "update");
-
-	        factoryBean.setJpaProperties(props);
-
-	        factoryBean.setPackagesToScan("br.com.samuel.cartorio.models");
-
-	        return factoryBean;
-	 }
 	
 }
--- src/main/java/br/com/samuel/cartorio/config/ServletSpringMVC.java
+++ src/main/java/br/com/samuel/cartorio/config/ServletSpringMVC.java
@@ -1,7 +1,10 @@
 package br.com.samuel.cartorio.config;
 
 import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
+import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
@@ -15,7 +18,7 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-	    return new Class[] {AppWebConfiguration.class, JPAConfiguration.class};
+	    return new Class[] {AppWebConfiguration.class,  JPAConfiguration.class, JPAProductionConfiguration.class};
 	}
 
 	@Override
@@ -29,5 +32,11 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 	        encodingFilter.setEncoding("UTF-8");
 	        return new Filter[] {encodingFilter};
 	    }
+		@Override
+		public void onStartup(ServletContext servletContext) throws ServletException {
+			super.onStartup(servletContext);
+			servletContext.addListener(RequestContextListener.class);
+			servletContext.setInitParameter("spring.profiles.active", "dev");
+		}
 
 }
